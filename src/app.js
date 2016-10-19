f75ded43877ecdf29ef4f0b132265323ae6ff0cf
Rahul Rayaprolu <rayaprolurahul1991@gmail.com>
2016-10-19 15:06:51 +0530

    adds styles.module.css and app.css and imports them in app.js also includes some loaders in webpack.config.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import styles from './styles.module.css'
+import './app.css'
+
 const App = React.createClass({
-    render: function () {
-        return (<div>Text text text</div>)
-    }
+  render: function () {
+    return (<div className={styles.wrapper} >Text text text</div>)
+  }
 })
 const mountNode = document.querySelector('#root')
 ReactDOM.render(<App />, mountNode)
