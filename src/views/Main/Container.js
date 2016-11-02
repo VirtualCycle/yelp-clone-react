5192ff3de14103709f3e4ed6056a007e6a002b88
Rahul Rayaprolu <rayaprolurahul1991@gmail.com>
2016-11-02 11:45:40 +0530

    add sidebar component

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -3,6 +3,7 @@ import Map, {GoogleApiWrapper} from 'google-maps-react'
 
 import {searchNearby} from 'utils/googleApiHelpers'
 import Header from 'components/Header/Header'
+import Sidebar from 'components/Sidebar/Sidebar'
 import styles from './styles.module.css'
 
 export class Container extends React.Component {
@@ -34,21 +35,21 @@ export class Container extends React.Component {
   }
   render () {
     return (
-      <div>
-        <Map
-          onReady={this.onReady.bind(this)}
-          google={this.props.google}
-          visible={false}
-          className={styles.wrapper}>
-          <Header />
-          <div className={styles.content}>
-
-            {this.state.places.map(place => {
-              return (<div key={place.id}>{place.name}</div>)
-            })}
-          </div>
-        </Map>
-      </div>
+      <Map
+        onReady={this.onReady.bind(this)}
+        google={this.props.google}
+        visible={false}
+        className={styles.wrapper}>
+        <Header />
+        <Sidebar
+          title={'Restaurants'}
+          places={this.state.places} />
+        <div className={styles.content}>
+          {this.state.places.map(place => {
+            return (<div key={place.id}>{place.name}</div>)
+          })}
+        </div>
+      </Map>
     )
   }
 }
